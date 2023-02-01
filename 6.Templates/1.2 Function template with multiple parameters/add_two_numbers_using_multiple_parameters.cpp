526ead4e9d87abf6e763f5a26adfd1dd274bd6dd
Sunil Nath <sunil.211443@ncit.edu.np>
2023-02-01 16:40:40 +0545

    program added

--- 6.Templates/1.2 Function template with multiple parameters/add_two_numbers_using_multiple_parameters.cpp
+++ 6.Templates/1.2 Function template with multiple parameters/add_two_numbers_using_multiple_parameters.cpp
@@ -1,3 +1,18 @@
 /*WAP to add two numbers using multiple parameters*/
 #include<iostream>
 using namespace std;
+template <class T1, class T2>
+void add(T1 a, T2 b)
+{
+    cout<<"Addition of two numbers is: "<<(a+b)<<endl;
+}
+int main()
+{
+    int a=2, b=3;
+    float c=2.5, d=5.25;
+    cout<<"For interger numbers"<<endl;
+    add(a,b);
+    cout<<"For decimal numbers"<<endl;
+    add(c,d);
+    return 0;
+}
