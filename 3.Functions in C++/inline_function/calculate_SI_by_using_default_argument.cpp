0d1c56a83daffc2650b2aa3cd286c25d6b2d5fb9
Sunil Nath <sunil.211443@ncit.edu.np>
2023-02-03 07:03:01 +0545

    modified

--- 3.Functions in C++/inline_function/calculate_SI_by_using_default_argument.cpp
+++ 3.Functions in C++/inline_function/calculate_SI_by_using_default_argument.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-float interest (float p, float t, float r=10);
+float interest(float p, float t, float r = 10);
 int main()
 {
-    float SI,p,t;
-    cout<<"Enter the principal and time"<<endl;
-    cin>>p>>t;
-    SI=interest(p,t);
-    cout<<"Simple interest is"<<SI<<endl;
+    float SI, p, t;
+    cout << "Enter the principal and time" << endl;
+    cin >> p >> t;
+    SI = interest(p, t);
+    cout << "Simple interest is" << SI << endl;
 }
-float interest (float p,float t,float r)
+float interest(float p, float t, float r)
 {
-    return (p*t*r)/1000;
+    return (p * t * r) / 1000;
 }
