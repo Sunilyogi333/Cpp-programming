83eed38a3111bd9659a7cb8b938f449b43b235ea
Sunil Nath <sunil.211443@ncit.edu.np>
2023-02-03 07:01:15 +0545

    modified

--- 4.Inheritance: Extending classes/check.cpp
+++ 4.Inheritance: Extending classes/check.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class A
 {
-int v;
+    int v;
+
 public:
-int p;
+    int p;
 };
 int main()
 {
     A a;
-   // cout<<a.v<<endl; unaccessable because it's private date member;
-    cout<<a.p<<endl;
+    // cout<<a.v<<endl; unaccessable because it's private date member;
+    cout << a.p << endl;
     return 0;
 }
