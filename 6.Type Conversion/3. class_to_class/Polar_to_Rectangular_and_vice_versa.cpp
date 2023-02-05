db9e1a92b0f58f37fcdc1db9bf379569c6c81fed
Sunil Nath <sunil.211443@ncit.edu.np>
2023-02-05 16:29:36 +0545

    polar to rectangle

--- 6.Type Conversion/3. class_to_class/Polar_to_Rectangular_and_vice_versa.cpp
+++ 6.Type Conversion/3. class_to_class/Polar_to_Rectangular_and_vice_versa.cpp
@@ -1,17 +1,16 @@
-// WAP to convert Polar to rectangular coordinates by using constructors.
-#include <iostream>
+// WAP to convert Polar to rectangular coordinates and vice-versa
+#include<iostream>
 #include<math.h>
 using namespace std;
 class Polar
 {
     float radius, angle;
-
-public:
+    public:
     Polar(){}
     Polar(float r, float a)
     {
-        radius = r;
-        angle = a;
+        radius=r;
+        angle=a;
     }
     void display()
     {
@@ -29,13 +28,12 @@ public:
 class Rectangular
 {
     float xco, yco;
-
-public:
+    public:
     Rectangular(){}
     Rectangular(float x, float y)
     {
-        xco = x;
-        yco = y;
+        xco=x;
+        yco=y;
     }
     void display()
     {
@@ -43,19 +41,31 @@ public:
     }
     Rectangular (Polar P)
     {
-        float r=P.getr();
-        float a=P.geta();
+        float r= P.getr();
+        float a= P.geta();
         xco=r*cos(a);
         yco=r*sin(a);
+    }
+    operator Polar()
+    {
+        float r=sqrt(pow(xco,2)+pow(yco,2));
+        float a= atan(yco/xco);
+        return Polar(r,a);
     }
 };
 int main()
 {
-    Polar P(10.0,0.7853938);
-    P.display();
-    Rectangular R;
-    cout<<"After conversion, Rectagular Coordinates are: "<<endl;
-    R=P;
-    R.display();
+    Polar P1(10.0, 0.785398);
+    P1.display();
+    Rectangular R1;
+    R1=P1;
+    cout<<"After conversion"<<endl;
+    R1.display();
+    Rectangular R2(7.07107, 7.07107);
+    R2.display();
+    Polar P2;
+    P2=R2;
+    cout<<"After conversion"<<endl;
+    P2.display();
     return 0;
 }
